93d8dc1bfd26a26241eab3913d5f9b8aa2210120
Kamil Synowiec <1935002@brunel.ac.uk>
2022-09-29 12:35:32 +0100

    Update script.js

--- covidCases/script.js
+++ covidCases/script.js
@@ -3,7 +3,7 @@ async function fetchData(){
    const record=await res.json();
    document.getElementById("date").innerHTML=record.data[0].date;
    document.getElementById("areaName").innerHTML=record.data[0].areaName;
-   document.getElementById("latestBy").innerHTML=record.data[0].latesBy;
+   document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
 }
 fetchData();
